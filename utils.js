0e058ad860c17f4762870a0d3291b19dfdf7a9da
fengqinglingyu <liang823740098@hotmail.com>
2018-04-03 21:34:37 +0800

    Create utils.js

---
+++ utils.js
@@ -0,0 +1,109 @@
+'use strict';
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const packageConfig = require('../package.json');
+ 
+exports.assetsPath = function(_path) {
+  const assetsSubDirectory =
+    process.env.NODE_ENV === 'production' ? 'static' : 'static';
+ 
+  return path.posix.join(assetsSubDirectory, _path);
+};
+ 
+exports.cssLoaders = function(options) {
+  options = options || {};
+ 
+  const cssLoader = {
+    loader: 'css-loader',
+    options: {
+      sourceMap: options.sourceMap
+    }
+  };
+ 
+  const postcssLoader = {
+    loader: 'postcss-loader',
+    options: {
+      sourceMap: options.sourceMap
+    }
+  };
+ 
+  // generate loader string to be used with extract text plugin
+  function generateLoaders(loader, loaderOptions) {
+    const loaders = options.usePostCSS
+      ? [cssLoader, postcssLoader]
+      : [cssLoader];
+ 
+    if (loader) {
+      // loader的处理顺序是先处理反过来的，所以是push
+      loaders.push({
+        loader: loader + '-loader',
+        options: Object.assign({}, loaderOptions, {
+          sourceMap: options.sourceMap
+        })
+      });
+    }
+ 
+    // Extract CSS when that option is specified
+    // (which is the case during production build)
+    if (options.extract) {
+      return ExtractTextPlugin.extract({
+        use: loaders,
+        fallback: 'vue-style-loader' // <- 这是vue-loader的依赖，所以如果使用npm3，则不需要显式安装
+      });
+    } else {
+      return ['vue-style-loader'].concat(loaders);
+    }
+  }
+ 
+  // https://vue-loader.vuejs.org/en/configurations/extract-css.html
+  return {
+    css: generateLoaders(),
+    postcss: generateLoaders(),
+    less: generateLoaders('less'),
+    sass: generateLoaders('sass', { indentedSyntax: true }),
+    scss: generateLoaders('sass'),
+    stylus: generateLoaders('stylus'),
+    styl: generateLoaders('stylus')
+  };
+  /**
+   * {
+   *   test: /\.css$/,
+   *   use: ['css-loader', 'postcss-loader']
+   * }
+   */
+};
+ 
+// 生成独立样式文件的loader,指的是不在.vue文件里面的样式
+exports.styleLoaders = function(options) {
+  const output = [];
+  const loaders = exports.cssLoaders(options);
+ 
+  for (const extension in loaders) {
+    const loader = loaders[extension];
+    output.push({
+      test: new RegExp('\\.' + extension + '$'),
+      use: loader
+    });
+  }
+  console.log(output);
+  debugger;
+  return output;
+};
+ 
+exports.createNotifierCallback = () => {
+  const notifier = require('node-notifier');
+ 
+  return (severity, errors) => {
+    if (severity !== 'error') return;
+ 
+    const error = errors[0];
+    const filename = error.file && error.file.split('!').pop();
+ 
+    notifier.notify({
+      title: packageConfig.name,
+      message: severity + ': ' + error.name,
+      subtitle: filename || '',
+      icon: path.join(__dirname, 'logo.png')
+    });
+  };
+};
